08020148d72f69f8f1102567c18c128f74cb7b5c
Delancey <dsdelancey@gmail.com>
2020-06-26 10:46:47 -0400

    add ability to edit and delete posts, added external db file

--- src/main/java/com/win/techtalentblog/BlogPost/BlogPost.java
+++ src/main/java/com/win/techtalentblog/BlogPost/BlogPost.java
@@ -22,6 +22,15 @@ public class BlogPost {
         this.blogEntry = blogEntry;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    // public void setId(Long id) {
+    //     this.id = id;
+    // }
+
+    
     public String getTitle() {
         return title;
     }
@@ -51,6 +60,7 @@ public class BlogPost {
         return "BlogPost [author=" + author + ", blogEntry=" + blogEntry + ", id=" + id + ", title=" + title + "]";
     }
 
+    
     
 
 }
